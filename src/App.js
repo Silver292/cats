c3aa1e3c7db0cdf0dd01fd1bc991cc83b46cbe16
Tom Scott <tom.scott@cantarus.com>
2022-02-17 21:46:18 +0000

    Basket work.

--- src/App.js
+++ src/App.js
@@ -39,12 +39,24 @@ function App() {
 
       <div className={`fixed inset-0 bg-white w-56 p-3 pt-28 transition-transform ${showBasket ? 'translate-x-0' : '-translate-x-full'}`}>
         <h2 className='text-gray-600 text-xl font-bold align-center'>Your Basket</h2>
-        {basket.map(item => <div key={item.id}>
-          <p>{item.name}</p>
-          <button onClick={() => removeFromBasket(item)}>Remove</button>
+        {basket.map(item => <div key={item.id} className='grid grid-cols-2 my-3'>
+          <div className='flex items-center'>
+            <img className='inline object-cover w-6 h-6 rounded-full mr-2' src={item.imageUrl} alt={item.name} />
+            <p>{item.name}</p>
+          </div>
+          <p className='text-right'>£{item.price}</p>
+          <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6 col-start-2 text-red-600 border border-red-300 rounded justify-self-end mt-2' fill='none' viewBox='0 0 24 24' stroke='currentColor' onClick={() => removeFromBasket(item)}>
+            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16' />
+          </svg>
         </div>)}
 
-        {basket.length ? (<div><h3>total: £{basket.reduce((prev, curr) => prev + parseFloat(curr.price), 0.0).toFixed(2)}</h3></div>) : null}
+        {basket.length ? (
+          <div className='grid grid-cols-2 my-3'>
+            <h3>Total:</h3>
+            <h3 className='text-right'>£{basket.reduce((prev, curr) => prev + parseFloat(curr.price), 0.0).toFixed(2)}</h3>
+          </div>)
+          :
+          null}
       </div>
 
 
